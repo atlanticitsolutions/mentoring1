bbf1feee5656e8aae1b17d1ef14f57e686cecc67
Atlantic IT Solutions <info@atlanticitsolutions.com>
2024-01-16 09:56:08 -0500

    MentorPage addMentee form working

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { collection, getDocs, doc, deleteDoc } from 'firebase/firestore'
+import { collection, getDocs, addDoc } from 'firebase/firestore'
 import { db } from '../config/firebase-config'
 import { Link } from 'react-router-dom'
 
 export default function Dashboard() {
   const [mentors, setMentors] = useState([])
+  const [fname, setFname] = useState('')
   const mentorsCollectionRef = collection(db, 'mentors')
 
   const getMentors = async () => {
@@ -21,12 +22,35 @@ export default function Dashboard() {
     }
   }
 
+  const addMentor = async (e) => {
+    e.preventDefault()
+    try {
+      await addDoc(mentorsCollectionRef, {
+        firstName: fname,
+      })
+      getMentors()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   useEffect(() => {
     getMentors()
   }, [])
 
   return (
     <div>
+      <h1 className='text-3xl'>Add Mentor</h1>
+      <form>
+        <input
+          type='text'
+          name='firstName'
+          placeholder='First Name...'
+          onChange={(e) => setFname(e.target.value)}
+        />
+        <button onClick={(e) => addMentor(e)}>Add</button>
+      </form>
+
       <h1 className='text-3xl'>Mentors</h1>
       {mentors.map((mentor) => (
         <Link to={`/mentor/${mentor.id}`} key={mentor.id}>
--- src/pages/MentorPage.jsx
+++ src/pages/MentorPage.jsx
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { doc, getDoc } from 'firebase/firestore'
+import { doc, getDoc, collection, addDoc } from 'firebase/firestore'
 import { db } from '../config/firebase-config'
 
 export default function MentorPage() {
   const { id } = useParams()
   const [mentor, setMentor] = useState()
   const [loading, setLoading] = useState(true)
+  const docRef = doc(db, 'mentors', id)
+  const [mentees, setMentees] = useState([])
+  const [first, setFirst] = useState('')
+  const menteesCollectionRef = collection(db, `mentors/${id}/mentees`)
+
+  const addMentee = async (e) => {
+    e.preventDefault()
+    try {
+      await addDoc(menteesCollectionRef, {
+        fName: first,
+      })
+      //   getMentors()
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const getMentor = async () => {
-    const docRef = doc(db, 'mentors', id)
-
     try {
       const docSnap = await getDoc(docRef)
       if (docSnap.exists()) {
@@ -30,13 +44,27 @@ export default function MentorPage() {
   }, [])
 
   return (
-    <div>
-      <div>Mentor ID: {id}</div>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <div>Mentor Name: {mentor.firstName} </div>
-      )}
-    </div>
+    <>
+      <div>
+        <div>Mentor ID: {id}</div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div>Mentor Name: {mentor.firstName} </div>
+        )}
+      </div>
+
+      <h1 className='text-3xl'>Add Mentees</h1>
+      <form>
+        <input
+          type='text'
+          name='first'
+          placeholder='Mentee First Name...'
+          onChange={(e) => setFirst(e.target.value)}
+        />
+        <button onClick={(e) => addMentee(e)}>Add Mentee</button>
+      </form>
+      <h1 className='text-3xl'>Mentees</h1>
+    </>
   )
 }
